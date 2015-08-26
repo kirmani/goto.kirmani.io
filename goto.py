416934cbf8b9de59499eb105ea5c26130ce77c51
Sean Kirmani <sean@kirmani.io>
2015-08-26 17:39:15 +0000

    Added link counter

--- goto.py
+++ goto.py
@@ -11,9 +11,11 @@ from forms import ShortLinkForm
 from flask import Flask,redirect
 from flask import render_template
 from python import tools
+from python import shortlink
 import json
 
-SHORT_LINK_DATA_FILE = os.path.join(os.path.dirname(__file__),'shortlinks.json')
+SHORT_LINK_DATA_FILE = os.path.join(os.path.dirname(__file__),
+    'data/shortlinks.json')
 
 app = Flask(__name__)
 app.secret_key = 'kirmani_io_secret_key'
@@ -27,17 +29,23 @@ def hello():
     short_links = _LoadShortLinks()
     if form.short_link.data in short_links:
       return 'Shortlink already exists.'
-    short_links[form.short_link.data] = form.url.data
+    short_links[form.short_link.data] = {
+        shortlink.Shortlink.URL: form.url.data,
+        shortlink.Shortlink.COUNT: 0
+    }
     _WriteShortLinks(short_links)
     return 'Shortlink added.'
   return render_template('index.html', form=form)
 
-@app.route('/<shortlink>')
-def RedirectShortLink(shortlink=None):
+@app.route('/<shortlink_query>')
+def RedirectShortLink(shortlink_query=None):
   short_links = _LoadShortLinks()
-  if shortlink not in short_links:
+  if shortlink_query not in short_links:
     return 'Shortlink not found'
-  return redirect(short_links[shortlink], code=302)
+  short_links[shortlink_query][shortlink.Shortlink.COUNT] += 1
+  _WriteShortLinks(short_links)
+  return redirect(short_links[shortlink_query][shortlink.Shortlink.URL],
+      code=302)
 
 def _LoadShortLinks():
   try:
